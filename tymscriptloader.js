5d14b322d3cc3e1bf7ab09756558c84172a013a4
Théotime Loiseau <theotime.loiseau@gmail.com>
2017-09-03 16:34:15 +0200

    Support CSS files

--- tymscriptloader.js
+++ tymscriptloader.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 Théotime Loiseau
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 (function() {
     var scripts = {};
@@ -91,12 +106,33 @@
         document.body.appendChild(el);
     }
 
+    var createLink = function(script) {
+        var el = document.createElement('link');
+        el.addEventListener('load', function() {
+            onScriptLoad(script);
+        });
+        el.href = script.source;
+        el.setAttribute('rel', 'stylesheet');
+        el.setAttribute('type','text/css');
+
+        if (script.options) {
+            for (option in script.options) {
+                el.setAttribute('option', script.options[option]);
+            }
+        }
+
+        document.head.appendChild(el);
+    }
+
     var loadScripts = function() {
         var noDependencyScripts = [];
         prepareLoad(noDependencyScripts);
-
         for (var i = 0, l = noDependencyScripts.length; i < l; i++) {
-            createScript(scripts[noDependencyScripts[i]]);
+            if (scripts[noDependencyScripts[i]].type === 'js') {
+                createScript(scripts[noDependencyScripts[i]]);
+            } else if (scripts[noDependencyScripts[i]].type === 'link') {
+                createLink(scripts[noDependencyScripts[i]]);
+            }
         }
     };
 
